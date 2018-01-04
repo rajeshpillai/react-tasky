5101af167ad04c209062cfbace0a24dc5308b734
rajesh.pillai <rajesh.pillai@kohls.com>
2018-01-03 21:38:07 -0800

    Added missing Menu and projectList

---
+++ src/Components/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Menu = () => {
+	return (
+		<div className="main-menu">
+			<ul >
+				<li><a href="#">Projects</a></li>
+				<li><a href="#">Dashboard</a></li>
+				<li><a href="#">Admin</a></li>
+			</ul>
+		</div>
+	);
+}
+
+export default Menu;
---
+++ src/Components/ProjectList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Project = (props) => {
+	var {projects} = props;
+
+	var projectView = projects.map((project) => {
+		return (
+			<li className="project" key={project.id}>
+				<div className="project-header"><a href="#">{project.title}</a></div>
+
+				<div className="project-footer">&copy;</div>
+
+			</li>
+		);	
+	});
+	return (
+		<ul className="project-list">
+			{projectView}
+		</ul>
+	);	
+}
+
+export default Project;
